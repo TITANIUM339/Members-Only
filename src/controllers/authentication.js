94573a2309cb15eb6b6ff43604e947b321c67837
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-09-21 15:48:40 +0300

    Signup user

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -1,7 +1,55 @@
+import { validateSignup } from "../helpers/validation.js";
+import { validationResult, matchedData } from "express-validator";
+import asyncHandler from "express-async-handler";
+import { users } from "../models/queries.js";
+import bcrypt from "bcryptjs";
+
 const signup = {
     get(req, res) {
         res.render("signup");
     },
+    post: [
+        validateSignup(),
+        asyncHandler(async (req, res, next) => {
+            const error = validationResult(req);
+
+            if (!error.isEmpty()) {
+                res.status(400).render("signup", { errors: error.array() });
+                return;
+            }
+
+            const { username, firstName, lastName, password, adminPassword } =
+                matchedData(req);
+
+            const admin = adminPassword ? true : false;
+
+            const passwordHash = await bcrypt.hash(password, 10);
+
+            const id = await users.add(
+                username,
+                firstName,
+                lastName,
+                passwordHash,
+                admin,
+            );
+
+            const user = {
+                id,
+                first_name: firstName,
+                last_name: lastName,
+                admin,
+            };
+
+            req.login(user, (err) => {
+                if (err) {
+                    next(err);
+                    return;
+                }
+
+                res.redirect("/");
+            });
+        }),
+    ],
 };
 
 export { signup };
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { signup } from "../controllers/authentication.js"
+import { signup } from "../controllers/authentication.js";
 
 const router = Router();
 
-router.route("/sign-up").get(signup.get);
+router.route("/sign-up").get(signup.get).post(signup.post);
 
 export default router;
