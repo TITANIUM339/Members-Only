36f432e2c5ec4a6ef4399b6ef8afd465d73722e3
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-09-22 17:40:04 +0300

    Render new-club page

--- src/controllers/clubs.js
+++ src/controllers/clubs.js
@@ -1,10 +1,27 @@
 import { clubs } from "../models/queries.js";
 import asyncHandler from "express-async-handler";
 
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.redirect("/log-in");
+    }
+}
+
 const index = {
     get: asyncHandler(async (req, res) => {
         res.render("index", { clubs: await clubs.getAll() });
     }),
 };
 
-export { index };
+const newClub = {
+    get: [
+        isAuthenticated,
+        (req, res) => {
+            res.render("newClub");
+        }
+    ]
+}
+
+export { index, newClub };
--- src/routes/clubs.js
+++ src/routes/clubs.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { index } from "../controllers/clubs.js";
+import { index, newClub } from "../controllers/clubs.js";
 
 const router = Router();
 
 router.get("/", index.get);
+
+router.route("/clubs/new-club").get(newClub.get);
 
 export default router;
