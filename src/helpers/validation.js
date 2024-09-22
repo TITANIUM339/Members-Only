38afcc7258d268d0ffc2c8b264ecb37d8849789b
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-09-22 09:20:37 +0300

    Simple refactor

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,18 +1,6 @@
 import { body } from "express-validator";
 import { users } from "../models/queries.js";
 
-function validateUsername() {
-    return body("username")
-        .trim()
-        .customSanitizer((value) => value.replaceAll(/\s+/g, " "))
-        .notEmpty()
-        .withMessage("Must provide a username.")
-        .isAlphanumeric()
-        .withMessage("Username must be alpha-numeric.")
-        .isLength({ max: 8 })
-        .withMessage("Username must be within 8 characters.");
-}
-
 function validateName(field, name) {
     const fieldName = `${name.charAt(0).toUpperCase()}${name.slice(1)}`;
 
@@ -27,20 +15,25 @@ function validateName(field, name) {
         .withMessage(`${fieldName} must be within 8 characters.`);
 }
 
-function validatePassword() {
-    return body("password").notEmpty().withMessage("Must provide a password.");
-}
-
 function validateSignup() {
     return [
-        validateUsername().custom(async (value) => {
-            if (await users.getByUsername(value)) {
-                throw new Error("Username already exits.");
-            }
-        }),
+        body("username")
+            .trim()
+            .customSanitizer((value) => value.replaceAll(/\s+/g, " "))
+            .notEmpty()
+            .withMessage("Must provide a username.")
+            .isAlphanumeric()
+            .withMessage("Username must be alpha-numeric.")
+            .isLength({ max: 8 })
+            .withMessage("Username must be within 8 characters.")
+            .custom(async (value) => {
+                if (await users.getByUsername(value)) {
+                    throw new Error("Username already exits.");
+                }
+            }),
         validateName("firstName", "first name"),
         validateName("lastName", "last name"),
-        validatePassword(),
+        body("password").notEmpty().withMessage("Must provide a password."),
         body("confirmPassword")
             .notEmpty()
             .withMessage("Must confirm password.")
