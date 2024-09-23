82bc3158e9487da2c17a5b1e43ce9fbd559a5548
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-09-23 14:32:25 +0300

    Create new-club validation chain

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,5 +1,5 @@
 import { body } from "express-validator";
-import { users } from "../models/queries.js";
+import { clubs, users } from "../models/queries.js";
 
 function validateName(field, name, maxLength) {
     const nameCapital = `${name.charAt(0).toUpperCase()}${name.slice(1)}`;
@@ -31,7 +31,7 @@ function validateSignup() {
     return [
         validateTextInput("username", "username", 8).custom(async (value) => {
             if (await users.getByUsername(value)) {
-                throw new Error("Username already exits.");
+                throw new Error("Username already exists.");
             }
         }),
         validateName("firstName", "first name", 8),
@@ -49,4 +49,21 @@ function validateSignup() {
     ];
 }
 
-export { validateSignup };
+function validateNewClub() {
+    return [
+        validateTextInput("title", "title", 16).custom(async (value) => {
+            if (await clubs.getByName(value)) {
+                throw new Error("Club already exists.");
+            }
+        }),
+        body("description")
+            .optional()
+            .trim()
+            .customSanitizer((value) => value.replaceAll(/\s+/g, " ")),
+        body("clubPassword")
+            .notEmpty()
+            .withMessage("Must provide a club password"),
+    ];
+}
+
+export { validateSignup, validateNewClub };
